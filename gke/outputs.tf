ff87fb73556573ea072de0866c8febe10c3687ca
Mikael Sennerholm <mikael@sennerholm.net>
2017-08-24 21:53:41 +0200

    Output endpoint information to connect to the cluster

--- gke/outputs.tf
+++ gke/outputs.tf
@@ -2,14 +2,26 @@
 output "endpoint" {
   value = "https://${google_container_cluster.gke.endpoint}"
 }
-output "client_cert" {
-  value = "https://${google_container_cluster.gke.master_auth.0.client_certificate}"
+
+output "client_certificate" {
+  value = "${base64decode(google_container_cluster.gke.master_auth.0.client_certificate)}"
 }
 
 output "client_key" {
-  value = "https://${google_container_cluster.gke.master_auth.0.client_key}"
+  value = "${base64decode(google_container_cluster.gke.master_auth.0.client_key)}"
 }
 
 output "ca_certificate" {
-  value = "https://${google_container_cluster.gke.master_auth.0.ca_certificate}"
+  value = "${base64decode(google_container_cluster.gke.master_auth.0.cluster_ca_certificate)}"
 }
+
+
+output "username" {
+  value = "${google_container_cluster.gke.master_auth.0.username}"
+}
+
+
+output "password" {
+  value = "${google_container_cluster.gke.master_auth.0.password}"
+}
+
