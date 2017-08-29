faecea31af82d66a7e05fe8d5b9352ea2b650d4e
Mikael Sennerholm <mikael@sennerholm.net>
2017-08-29 16:00:00 +0200

    Have some hardcoded prod stuff, but is starting

--- gocd-server/main.tf
+++ gocd-server/main.tf
@@ -1,28 +1,3 @@
-data "terraform_remote_state" "gke" {
-  backend = "gcs"
-  config {
-    bucket = "${var.google_project}"
-    path = "europe-west1/prod/gke/terraform.tfstate"    
-    project="${var.google_project}" 
-    credentials = "${var.google_keyfile}"
-  }
-}
-
-provider "kubernetes" {
-  host = "${data.terraform_remote_state.gke.endpoint}"
-  username = "${data.terraform_remote_state.gke.username}"
-  password = "${data.terraform_remote_state.gke.password}"
-  client_certificate = "${data.terraform_remote_state.gke.client_certificate}"
-  client_key = "${data.terraform_remote_state.gke.client_key}"
-  cluster_ca_certificate = "${data.terraform_remote_state.gke.ca_certificate}"
-}
-
-provider "google" {
-  project     = "${var.google_project}"
-  region      = "${var.google_region}"
-  credentials = "${file("${var.google_keyfile}")}"
-}
-
 
 resource "kubernetes_namespace" "gocd-server" {
   metadata {
@@ -30,18 +5,52 @@ resource "kubernetes_namespace" "gocd-server" {
   }
 }
 
-resource "google_compute_disk" "go_data" {
+resource "google_compute_disk" "prod_go_data" {
   name  = "prod-go-data"
   type  = "pd-standard"
   zone  = "${var.gce_zone}"
   size  = "100"
 }
 
-resource "google_compute_disk" "go_datadb" {
+resource "google_compute_disk" "prod_go_datadb" {
   name  = "prod-go-data-db"
   type  = "pd-ssd"
   zone  = "${var.gce_zone}"
   size  = "10"
 }
 
-// Todo Add loadbalancer to reach gocd-server from agents.
+// We need an initcontainer to fill the go_data disk with bootstrap config. 
+// Otherwize we should have declared the pod as a kubectl pod
+// Now we doesn't handle delete any good...
+resource "null_resource" "gocd-pod-with-init" {
+  # Changes to any instance of the cluster requires re-provisioning
+  triggers {
+    go_data = "${google_compute_disk.prod_go_data.self_link}"
+  }
+  depends_on = ["kubernetes_namespace.gocd-server",
+  				"google_compute_disk.prod_go_data",
+  				"google_compute_disk.prod_go_datadb"]
+  provisioner "local-exec" {
+    command = "kubectl apply --namespace gocd-server -f ${path.module}/goserver.yaml"
+  }
+}
+
+resource "kubernetes_service" "gocd-server" {
+  metadata {
+    name = "gocd-server"
+    namespace = "gocd-server"
+    
+  }
+  spec {
+    selector {
+      name = "gocd-server"
+    }
+    session_affinity = "None"
+    port {
+      port = 443
+      target_port = 8154
+    }
+    type = "LoadBalancer"
+  }
+}
+
--- gocd-server/outputs.tf
+++ gocd-server/outputs.tf
@@ -1,8 +1,5 @@
 // Output url to Go server, needed för agents to connect to.
 output "url"
 {
-	  value = "https://loadbalancerip:4503"
+	  value = "https://${kubernetes_service.gocd-server.load_balancer_ingress.0.ip}:443"
 }
-
-
-
