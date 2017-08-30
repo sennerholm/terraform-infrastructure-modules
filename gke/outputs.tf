cac61395e4f03cefd2f00476bd8349edceb7a5c7
Mikael Sennerholm <mikael@sennerholm.net>
2017-08-30 10:28:13 +0200

    Adding output, currently redundant, but removing when I have removed all references

--- gke/outputs.tf
+++ gke/outputs.tf
@@ -3,6 +3,16 @@ output "endpoint" {
   value = "https://${google_container_cluster.gke.endpoint}"
 }
 
+output "k8sconf" {
+  value = {
+  	endpoint 		   = "https://${google_container_cluster.gke.endpoint}"
+  	client_certificate = "${base64decode(google_container_cluster.gke.master_auth.0.client_certificate)}"
+  	client_key		   = "${base64decode(google_container_cluster.gke.master_auth.0.client_key)}"
+  	ca_certificate     = "${base64decode(google_container_cluster.gke.master_auth.0.cluster_ca_certificate)}"
+  	username		   = "${google_container_cluster.gke.master_auth.0.username}"
+  	password           = "${google_container_cluster.gke.master_auth.0.password}"
+  }
+}
 output "client_certificate" {
   value = "${base64decode(google_container_cluster.gke.master_auth.0.client_certificate)}"
 }
