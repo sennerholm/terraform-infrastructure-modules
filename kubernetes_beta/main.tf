559f9ca89dabdc9878ebae1b9f04cc3f6ae8c86a
Mikael Sennerholm <mikael@sennerholm.net>
2017-08-30 10:46:45 +0200

    Destroying also needs the kubeconfig file

--- kubernetes_beta/main.tf
+++ kubernetes_beta/main.tf
@@ -28,21 +28,22 @@ resource "null_resource" "kubernetes_resource" {
   // We need the files also when destroying, but I haven't find out the syntax for "when" if it's possible to 
   // declare them on both "destroy" and default
   provisioner "local-exec" {
+    command = "touch ${path.module}/kubeconfig"
+    when    = "destroy"
+  }
+  provisioner "local-exec" {
     command = "echo '${var.k8sconf["ca_certificate"]}' > ${path.module}/ca.pem"
     when    = "destroy"
-
   }
   provisioner "local-exec" {
     command = "echo '${var.k8sconf["client_certificate"]}' > ${path.module}/client.pem"
     when    = "destroy"
-
   }
   provisioner "local-exec" {
     command = "echo '${var.k8sconf["client_key"]}' > ${path.module}/client.key"
     when    = "destroy"
-
   }
- provisioner "local-exec" {
+  provisioner "local-exec" {
     when    = "destroy"
     command = "kubectl delete --kubeconfig=${path.module}/kubeconfig --server=${var.k8sconf["endpoint"]} --certificate-authority=${path.module}/ca.pem --client-certificate=${path.module}/client.pem --client-key=${path.module}/client.key --username=${var.k8sconf["username"]} --password=${var.k8sconf["password"]} --namespace=${var.namespace} -f - <<EOF\n${var.configuration}\nEOF"
   }
