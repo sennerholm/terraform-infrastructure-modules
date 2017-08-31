9f91314b7191c7e61f7f2be2a42c55d6ebfb5b2d
Mikael Sennerholm <mikael@sennerholm.net>
2017-08-31 11:39:35 +0200

    Adding auto_reg key, hardcoded at the moment

--- gocd-server/outputs.tf
+++ gocd-server/outputs.tf
@@ -3,3 +3,8 @@ output "url"
 {
 	  value = "https://${kubernetes_service.gocd-server.load_balancer_ingress.0.ip}:443"
 }
+// Hardcoded value, make it more dynamic in the future
+output "auto_reg"
+{
+	  value = "95e0a630-c1ed-4c16-b636-2450b30ec18a"
+}
