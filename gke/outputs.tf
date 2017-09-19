db6544201784cca3ce40b6ff20f2b2b4459567aa
Mikael Sennerholm <mikael@sennerholm.net>
2017-09-19 16:07:09 +0200

    Remove the old outputs of gke

--- gke/outputs.tf
+++ gke/outputs.tf
@@ -1,8 +1,4 @@
 
-output "endpoint" {
-  value = "https://${google_container_cluster.gke.endpoint}"
-}
-
 output "k8sconf" {
   value = {
   	endpoint 		   = "https://${google_container_cluster.gke.endpoint}"
@@ -13,25 +9,7 @@ output "k8sconf" {
   	password           = "${google_container_cluster.gke.master_auth.0.password}"
   }
 }
-output "client_certificate" {
-  value = "${base64decode(google_container_cluster.gke.master_auth.0.client_certificate)}"
+
+output "name" {
+  value = "${var.gke_name}"
 }
-
-output "client_key" {
-  value = "${base64decode(google_container_cluster.gke.master_auth.0.client_key)}"
-}
-
-output "ca_certificate" {
-  value = "${base64decode(google_container_cluster.gke.master_auth.0.cluster_ca_certificate)}"
-}
-
-
-output "username" {
-  value = "${google_container_cluster.gke.master_auth.0.username}"
-}
-
-
-output "password" {
-  value = "${google_container_cluster.gke.master_auth.0.password}"
-}
-
