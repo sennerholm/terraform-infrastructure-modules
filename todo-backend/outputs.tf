273757dafc22579cc284b2232f285362c217dcb1
Mikael Sennerholm <mikael@sennerholm.net>
2018-02-11 01:24:58 +0100

    Moved to src code

--- todo-backend/outputs.tf
+++
@@ -1,8 +0,0 @@
-output "url"
-{
-	  value = "http://${kubernetes_service.todo_backend.load_balancer_ingress.0.ip}"
-}
-
-output "google_datastore_namespace" {
-    value = "${kubernetes_namespace.todo_backend.metadata.0.name}"
-}
