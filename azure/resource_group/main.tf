c4d4a105dea32043cd3261a4337fc84a1d8ec82e
Mikael Sennerholm <mikael@sennerholm.net>
2019-11-05 19:44:07 +0100

    Change resource_group_name to resourcegroup_name

--- azure/resource_group/main.tf
+++ azure/resource_group/main.tf
@@ -1,5 +1,5 @@
 resource "azurerm_resource_group" "rg" {
-  name     = "${var.resource_group_name}-${var.location}"
+  name     = "${var.resourcegroup_name}-${var.location}"
   location = "${var.location}"
   tags = {
     subscription = "${var.subscription_name}"
--- azure/resource_group/variables.tf
+++ azure/resource_group/variables.tf
@@ -4,4 +4,4 @@ variable "client_secret" {}
 
 variable "subscription_name" {}
 variable location {}
-variable resource_group_name {}
+variable resourcegroup_name {}
