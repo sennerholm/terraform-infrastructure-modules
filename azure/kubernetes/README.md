afed824396f921b4a18ce12029f712b09ccc8f34
Mikael Sennerholm <mikael@sennerholm.net>
2019-11-05 19:44:33 +0100

    First version on azure kubernetes module

---
+++ azure/kubernetes/README.md
@@ -0,0 +1,2 @@
+Based on:
+https://docs.microsoft.com/sv-se/azure/terraform/terraform-create-k8s-cluster-with-tf-and-aks
---
+++ azure/kubernetes/variables.tf
@@ -0,0 +1,22 @@
+variable "client_id" {}
+variable "client_secret" {}
+
+variable "agent_count" {
+  default = 3
+}
+
+variable "ssh_public_key" {
+  default = "~/.ssh/id_rsa.pub"
+}
+
+variable "dns_prefix" {
+  default = "kubernetes"
+}
+
+variable cluster_name {
+  default = "kubernetes"
+}
+
+variable "subscription_name" {}
+variable location {}
+variable k8s_resourcegroup_name {}
