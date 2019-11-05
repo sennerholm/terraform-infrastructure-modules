21954002f40400bc10deac7524fc69d81459d2ac
Mikael Sennerholm <mikael@sennerholm.net>
2019-11-05 19:22:16 +0100

    Create a more unique name by appending location

--- azure/resource_group/main.tf
+++ azure/resource_group/main.tf
@@ -1,5 +1,5 @@
 resource "azurerm_resource_group" "rg" {
-  name     = "${var.resource_group_name}"
+  name     = "${var.resource_group_name}-${var.location}"
   location = "${var.location}"
   tags = {
     subscription = "${var.subscription_name}"
