e57024389bb097972aa426611833ebfbc0812669
Mikael Sennerholm <mikael@sennerholm.net>
2019-11-21 13:43:47 +0100

    Provkört

--- azure/containerregistry/main.tf
+++ azure/containerregistry/main.tf
@@ -1,6 +1,12 @@
 
-resource "azurerm_container_registry" "acr" {
-  name                = "${var.resource_name}"
+
+resource "random_integer" "gen_suffix" {
+  min = 1
+  max = 50000
+}
+
+resource "azurerm_container_registry" "registry" {
+  name                = "${var.resource_name}${random_integer.gen_suffix.result}${var.location}${var.subscription_name}"
   resource_group_name = "${var.full_resourcegroup_name}"
   location            = "${var.location}"
   sku                 = "Basic"
