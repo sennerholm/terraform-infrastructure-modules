6eca641c06da1410e3d25c3094889eaf6ccb5679
Mikael Sennerholm <mikael@sennerholm.net>
2019-11-22 11:14:42 +0100

    Remove storage_container and adding connectionstring

--- azure/storage_container/main.tf
+++ azure/storage_container/main.tf
@@ -10,7 +10,8 @@ resource "azurerm_storage_account" "account" {
   resource_group_name      = "${var.full_resourcegroup_name}"
   location                 = "${var.location}"
   account_tier             = "Standard"
-  account_replication_type = "GRS"
+  account_kind             = "StorageV2"
+  account_replication_type = "LRS"
 
   tags = {
     application    = "terragrunt"
@@ -18,11 +19,11 @@ resource "azurerm_storage_account" "account" {
   }
 }
 
-resource "azurerm_storage_container" "container" {
-  name                  = "stcont${var.resource_name}${random_integer.account_suffix.result}"
-  storage_account_name  = "${azurerm_storage_account.account.name}"
-  container_access_type = "private"
-}
+#resource "azurerm_storage_container" "container" {
+#  name                  = "stcont${var.resource_name}${random_integer.account_suffix.result}"
+#  storage_account_name  = "${azurerm_storage_account.account.name}"
+#  container_access_type = "private"
+#}
 
 
 # TODO
--- azure/storage_container/output.tf
+++ azure/storage_container/output.tf
@@ -5,12 +5,17 @@ output "storage_account_name" {
 }
 
 output "storage_account_key" {
+  sensitive   = true
   value       = "${azurerm_storage_account.account.primary_access_key}"
   description = "The AzureStorageAccountKey (primary)"
 }
+output "primary_connection_string" {
+  value     = "${azurerm_storage_account.account.primary_connection_string}"
+  sensitive = true
 
-output "storage_container" {
-  value       = "${azurerm_storage_container.container.name}"
-  description = "The name of the container to use"
 }
+#output "storage_container" {
+#  value       = "${azurerm_storage_container.container.name}"
+#  description = "The name of the container to use"
+#}
 
